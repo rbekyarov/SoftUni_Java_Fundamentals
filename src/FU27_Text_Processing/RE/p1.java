126a4c242f7c64d3ca39bc04cdaac1d4c2045f9a
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-11-10 18:36:34 +0200

    Text Processing

--- src/FU27_Text_Processing/RE/p1.java
+++ src/FU27_Text_Processing/RE/p1.java
@@ -8,12 +8,12 @@ public class p1 {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("end")) {
-            String result = "";
+            StringBuilder result = new StringBuilder();
             for (int i = input.length() - 1; i >= 0; i--) {
-                result += input.charAt(i);
+                result.append(input.charAt(i));
 
             }
-            System.out.printf("%s = %s%n", input, result);
+            System.out.printf("%s = %s%n", input, result.toString());
 
             input = scanner.nextLine();
         }
