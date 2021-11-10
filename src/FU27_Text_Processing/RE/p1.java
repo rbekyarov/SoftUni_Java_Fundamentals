3cb44dc131f286d42d50725234ddd26e8af77b2e
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-11-10 18:34:17 +0200

    Text Processing

--- src/FU27_Text_Processing/RE/p1.java
+++ src/FU27_Text_Processing/RE/p1.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 public class p1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String input = scanner.nextLine();
-
         while (!input.equals("end")) {
             String result = "";
             for (int i = input.length() - 1; i >= 0; i--) {
                 result += input.charAt(i);
 
-            }System.out.printf("%s = %s%n", input, result);
+            }
+            System.out.printf("%s = %s%n", input, result);
 
             input = scanner.nextLine();
         }
