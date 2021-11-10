b74637f8fea6b5f8ef858d9311791a31e6f3446c
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-11-10 20:53:44 +0200

    Text Processing

---
+++ src/FU27_Text_Processing/RE/p3.java
@@ -0,0 +1,21 @@
+package FU27_Text_Processing.RE;
+
+import java.util.Scanner;
+
+public class p3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String toRemove = scanner.nextLine();
+        String text = scanner.nextLine();
+
+        int index = text.indexOf(toRemove);
+
+        while (index != -1) {
+            text = text.replace(toRemove, "");
+            index = text.indexOf(toRemove);
+        }
+
+        System.out.println(text);
+    }
+}
