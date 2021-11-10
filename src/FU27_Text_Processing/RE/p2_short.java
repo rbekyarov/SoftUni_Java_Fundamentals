f3924d9da61a45d016177595b1f6414049601bc3
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-11-10 20:13:54 +0200

    Text Processing

--- src/FU27_Text_Processing/RE/p2_short.java
+++ src/FU27_Text_Processing/RE/p2_short.java
@@ -2,15 +2,14 @@ package FU27_Text_Processing.RE;
 
 import java.util.Scanner;
 
-public class p2 {
+public class p2_short {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String[] input = scanner.nextLine().split(" ");
         StringBuilder result = new StringBuilder();
-        for (String e: input){
-                result.append(e.repeat(e.length()));
-
+        for (String e : input) {
+            result.append(e.repeat(e.length()));
         }
         System.out.println(result);
     }
