d4d126ffdfe64be71e5ed3abd7b2a1a258b63576
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-11-23 15:29:48 +0200

    Regular Expressions Exercise

--- src/FU31_Exercise_Regular_Expressions/p4StarEnigma.java
+++ src/FU31_Exercise_Regular_Expressions/p4StarEnigma.java
@@ -1,70 +1,78 @@
 package FU31_Exercise_Regular_Expressions;
 
-
-import java.util.*;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-    public class p4StarEnigma {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int countMessages = Integer.parseInt(scanner.nextLine());
+public class p4StarEnigma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, Integer> attackedPlanets = new TreeMap<>();
+        Map<String, Integer> destroyedPlanets = new TreeMap<>();
+
+
+        for (int i = 0; i < n; i++) {
+            String cryptMessage = scanner.nextLine();
+            int countLetter = 0; //[s, t, a, r]
+            for (char letter : cryptMessage.toLowerCase().toCharArray()) {
+                if (letter == 's' || letter == 't' || letter == 'a' || letter == 'r') {
+                    countLetter++;
+                }
+            }
+            StringBuilder decryptedMessageBuilder = new StringBuilder();
+            for (char letter : cryptMessage.toCharArray()) {
+
+                letter -= countLetter;
+                decryptedMessageBuilder.append(letter);
+            }
+            String decryptedMessage = decryptedMessageBuilder.toString();
             String regex = "@(?<planetName>[A-Za-z]+)[^@!:>-]*:(?<population>[0-9]+)[^@!:>-]*!(?<attack>[AD])![^@!:>-]*->(?<soldierCount>[0-9]+)";
             Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(decryptedMessage);
+            if (matcher.find()) {
+                String planetName = matcher.group("planetName");
+                int population = Integer.parseInt(matcher.group("population"));
+                String attack = matcher.group("attack");
+                int soldierCount = Integer.parseInt(matcher.group("soldierCount"));
 
-            List<String> attackersPlanets = new ArrayList<>(); //списък с планетите с тип на атака A
-            List<String> destroyedPlanets = new ArrayList<>(); //списък с планетите с тип на атака D
-            for (int messageCount = 1; messageCount <= countMessages; messageCount++) {
-                String encryptedMessage = scanner.nextLine(); //криптирано съобщение
-                String decryptedMessage = getDecryptedMessage(encryptedMessage); //декриптирано
-                //декриптирано: PQ@Alderaa1:30000!A!->20000
-                Matcher matcher = pattern.matcher(decryptedMessage);
-                if (matcher.find()) {
-                    String planetName = matcher.group("planetName");
-                    //int population = Integer.parseInt(matcher.group("population"));
-                    String attackType = matcher.group("attack");  ; //A -> attacker; D - > destroyed
-                    //int soldierCount = Integer.parseInt(matcher.group("soldierCount"));
-                    if (attackType.equals("A")) {
-                        attackersPlanets.add(planetName);
-                    } else if (attackType.equals("D")) {
-                        destroyedPlanets.add(planetName);
+                if (attack.equals("A")) {
+                    if (!attackedPlanets.containsKey(planetName)) {
+                        attackedPlanets.put(planetName, 1);
+                    } else {
+                        int currentAttacks = attackedPlanets.get(planetName);
+                        attackedPlanets.put(planetName, 1 + currentAttacks);
+                    }
+                } else {
+                    if (!destroyedPlanets.containsKey(planetName)) {
+                        destroyedPlanets.put(planetName, 1);
+                    } else {
+                        int currentDestroyed = destroyedPlanets.get(planetName);
+                        destroyedPlanets.put(planetName, 1 + currentDestroyed);
                     }
                 }
             }
-
-            System.out.println("Attacked planets: " + attackersPlanets.size());
-            Collections.sort(attackersPlanets);
-            attackersPlanets.forEach(planet -> System.out.println("-> " + planet));
-
-            System.out.println("Destroyed planets: " + destroyedPlanets.size());
-            Collections.sort(destroyedPlanets);
-            destroyedPlanets.forEach(planet -> System.out.println("-> " + planet));
+        }
+        int countAttacked = 0;
+        int countDestroyed = 0;
+        for (Map.Entry<String, Integer> entry : attackedPlanets.entrySet()) {
+            countAttacked += entry.getValue();
+        }
+        System.out.printf("Attacked planets: %d%n", countAttacked);
+        for (Map.Entry<String, Integer> entry : attackedPlanets.entrySet()) {
+            System.out.printf("-> %s%n", entry.getKey());
         }
 
-        private static String getDecryptedMessage(String encryptedMessage) {
-            //криптирано съобщение: STCDoghudd4=63333$D$0A53333
-            //брой на спец. букви: 3
-            //декриптирано съобщение:
-            //1. броя на [s, S, T, t, A, a, R, r] в криптираното съобщение
-            int countLetters = getSpecialLettersCount(encryptedMessage);
-            StringBuilder decryptedMessage = new StringBuilder();
-            //взимаме всяка буква от криптираното съобщение -> ascii
-            //ascii - countLetters -> ascii кода на нова буква
-            //нова буква -> добавяме към декриптираното съобщение
-            for (char symbol : encryptedMessage.toCharArray()) {
-                char newSymbol = (char)(symbol - countLetters);
-                decryptedMessage.append(newSymbol);
-            }
-            return decryptedMessage.toString();
+        for (Map.Entry<String, Integer> entry : destroyedPlanets.entrySet()) {
+            countDestroyed += entry.getValue();
+        }
+        System.out.printf("Destroyed planets: %d%n", countDestroyed);
+        for (Map.Entry<String, Integer> entry : destroyedPlanets.entrySet()) {
+            System.out.printf("-> %s%n", entry.getKey());
         }
 
-        private static int getSpecialLettersCount(String encryptedMessage) {
-            int counter = 0;
-            for (char symbol : encryptedMessage.toLowerCase().toCharArray()) {
-                if (symbol == 's' || symbol == 'a' || symbol == 't' || symbol == 'r') {
-                    counter++;
-                }
-            }
-            return counter;
-        }
     }
+}
